ab057d94ecefdf62b0ff9b861c19c2db9ad538a0
Hanna <hanna-marii.kriisa@voco.ee>
2022-09-21 11:41:41 +0300

    Ülesanne 2.1

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,20 +1,28 @@
 // Ülesanne 1.1
-console.log("Tere, maailm!")
+// console.log("Tere, maailm!") */
 
 // Ülesanne 1.2
+/*
 let aasta = 2016
 let president = "Kersti Kaljulaid"
 let lause_keskosa = ". aastal valiti Eesti presidendiks "
 let lause = aasta + lause_keskosa + president
 console.log(lause)
+ */
 
 // Ülesanne 1.3
+/*
 let alus = prompt("Sisesta alus: ")
 let astendaja = prompt("Sisesta astendaja: ")
 let vastus = alus ** astendaja
-console.log(vastus)
+
+// saab ka Math.pow
+
+ console.log(vastus)
+ */
 
 // Ülesanne 1.4
+/*
 let nimi = prompt("Sisestage oma nimi: ")
 let lubatud_kiirus = parseInt(prompt("Sisestage lubatud kiirus:"))
 let tegelik_kiirus = parseInt(prompt("Sisestage tegelik kiirus: "))
@@ -28,17 +36,22 @@ function trahviArvutus() {
     }
 }
 console.log(trahviArvutus())
+*/
 
 // Ülesanne 1.5
+/*
+
 let ainepunktid = prompt("Sisesta ainepunktide arv")
 let nadala_arv = prompt("Sisesta nädalate arv")
-let ainepunktid_kokku = ainepunktid*3
+let ainepunktid_kokku = ainepunktid*26
 let kokku = ainepunktid_kokku / nadala_arv
 console.log("Ainepunktide arv: " + ainepunktid)
 console.log("Nädalate arv: " + nadala_arv)
 console.log("Kokku: " + Math.ceil(kokku))
+*/
 
 // Ülesanne 1.6
+/*
 let inimeste_arv = prompt("Sisesta inimeste arv")
 let kohtade_arv = 30
 let busside_arv = Math.floor(inimeste_arv / kohtade_arv)
@@ -50,8 +63,21 @@ console.log("Kohtade arv ühes bussis: " + kohtade_arv)
 console.log("Busse telliti: " + busside_arv)
 console.log("Bussi mahtus: " + mahtus_peale)
 console.log("Maha jäi: " + maha_jaanud)
+*/
 
+// Ülesanne 2.1
+/*
+let ohutemperatuur = prompt("Sisesta õhutemperatuur")
+function temperatuur () {
+    if (ohutemperatuur > 4) {
+        console.log("Ei ole jäätumise ohtu")
+    } else if (ohutemperatuur <= 4) {
+        console.log("On jäätumise oht")
+    }
+}
 
+console.log(temperatuur())
+*/
 
 
 
